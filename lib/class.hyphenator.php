9713b0fa8fb05f6efa547530b7e74506bac63c14
Thomas Kaegi <tk@meeloo.ch>
2016-07-05 10:14:14 +0200

    Konfiguration eingefügt, Sprachen generell ergänzt

--- lib/class.hyphenator.php
+++ lib/class.hyphenator.php
@@ -28,9 +28,35 @@
 		
 		private static function getHyphenator($language) {
 			if (!isset(self::$hyphenators[$language])) {
+				$addon = rex_addon::get(__CLASS__);
+				
 				$h = \Org\Heigl\Hyphenator\Hyphenator::factory(null, self::getLocale($language));
 				$o = $h->getOptions();
-				$o->setHyphen('&shy;');
+				
+				if (!empty($addon->getConfig('hyphen'))) {
+					$o->setHyphen($addon->getConfig('hyphen'));
+				} else {
+					$o->setHyphen('&shy;');
+				}
+				
+				if (!empty($addon->getConfig('leftMin'))) {
+					$o->setLeftMin($addon->getConfig('leftMin'));
+				} else {
+					$o->setLeftMin(2);
+				}
+				
+				if (!empty($addon->getConfig('rightMin'))) {
+					$o->setRightMin($addon->getConfig('rightMin'));
+				} else {
+					$o->setRightMin(2);
+				}
+				
+				if (!empty($addon->getConfig('wordMin'))) {
+					$o->setWordMin($addon->getConfig('wordMin'));
+				} else {
+					$o->setWordMin(6);
+				}
+				
 				$h->setOptions($o);
 				self::$hyphenators[$language] = $h;
 			}
@@ -41,11 +67,14 @@
 		private static function getLocale($language) {
 			switch ($language) {
 				case 'de':
-					return 'de_DE';
+					$language = 'de_DE';
+				break;
 				case 'en':
-					return 'en_US';
+					$language = 'en_GB';
+				break;
 			}
-			//throw new ErrorException('Language not supported for hyphenation', ['language' => $language]);
+			
+			return $language;
 		}
 	}
 ?>
