6da2165e774b87ff3dd20b39382806a909cd48bd
Thomas Kaegi <tk@walderwerber.ch>
2017-07-20 10:47:29 +0200

    Bugfix

--- lib/class.hyphenator.php
+++ lib/class.hyphenator.php
@@ -6,7 +6,7 @@
 			//Start - save html-tags
 				preg_match_all('/<[^<]*>/', $string, $matches, PREG_SET_ORDER);
 				foreach ($matches as $index => $match) {
-					$match[0] = str_replace(['?'], ['\?'], $match[0]);
+					$match[0] = str_replace(['?', '+', '@', '|'], ['\?', '\+', '\@', '\|'], $match[0]);
 					$string = preg_replace('|'.$match[0].'|', '###'.$index.'###', $string, 1);
 				}
 			//End - save html-attributes
