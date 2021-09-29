4c42c253e0f029371139d72c6d323490ca894740
schuer <ds@decaf.de>
2021-09-29 08:52:24 +0200

    code style

--- help.php
+++ help.php
@@ -1,22 +1,22 @@
 <?php
-	$code = "";
-	$code .= "<?php".PHP_EOL;
-	$code .= "	//Plaintext (aktuelle Sprache):".PHP_EOL;
-	$code .= "	echo hyphenator::hyphenate(REX_VALUE[id=1]);".PHP_EOL;
-	$code .= PHP_EOL;
-	$code .= "	//Markup (aktuelle Sprache):".PHP_EOL;
-	$code .= "	echo hyphenator::hyphenate(REX_VALUE[id=1 output=html]);".PHP_EOL;
-	$code .= PHP_EOL;
-	$code .= "	//Plaintext (andere Sprache):".PHP_EOL;
-	$code .= "	echo hyphenator::hyphenate(REX_VALUE[id=1], 'en');".PHP_EOL;
-	$code .= PHP_EOL;
-	$code .= "	//Markup (andere Sprache):".PHP_EOL;
-	$code .= "	echo hyphenator::hyphenate(REX_VALUE[id=1 output=html], 'en');".PHP_EOL;
-	$code .= "?>";
-	
-	$fragment = new rex_fragment();
-	$fragment->setVar('class', 'info', false);
-	$fragment->setVar('title', 'Beispiel: Module Output', false); //translate
-	$fragment->setVar('body', rex_string::highlight($code), false);
-	echo $fragment->parse('core/page/section.php');
-?>
+
+    $code = '';
+    $code .= '<?php'.PHP_EOL;
+    $code .= '	//Plaintext (aktuelle Sprache):'.PHP_EOL;
+    $code .= '	echo hyphenator::hyphenate(REX_VALUE[id=1]);'.PHP_EOL;
+    $code .= PHP_EOL;
+    $code .= '	//Markup (aktuelle Sprache):'.PHP_EOL;
+    $code .= '	echo hyphenator::hyphenate(REX_VALUE[id=1 output=html]);'.PHP_EOL;
+    $code .= PHP_EOL;
+    $code .= '	//Plaintext (andere Sprache):'.PHP_EOL;
+    $code .= "	echo hyphenator::hyphenate(REX_VALUE[id=1], 'en');".PHP_EOL;
+    $code .= PHP_EOL;
+    $code .= '	//Markup (andere Sprache):'.PHP_EOL;
+    $code .= "	echo hyphenator::hyphenate(REX_VALUE[id=1 output=html], 'en');".PHP_EOL;
+    $code .= '?>';
+
+    $fragment = new rex_fragment();
+    $fragment->setVar('class', 'info', false);
+    $fragment->setVar('title', 'Beispiel: Module Output', false); //translate
+    $fragment->setVar('body', rex_string::highlight($code), false);
+    echo $fragment->parse('core/page/section.php');
--- lib/class.hyphenator.php
+++ lib/class.hyphenator.php
@@ -1,87 +1,89 @@
 <?php
-	class hyphenator {
-		private static $hyphenators = array();
-				
-		public static function hyphenate($string, $language = '') {
-			//Start - save html-tags
-				preg_match_all('/<[^<]*>/', $string, $matches, PREG_SET_ORDER);
-				foreach ($matches as $index => $match) {
-					$string = preg_replace('|'.preg_quote($match[0],'|').'|', '###'.$index.'###', $string, 1);
-				}
-			//End - save html-attributes
-			
-			if ($language == '') {
-				$language = rex_clang::getCurrent()->getCode();
-			}
-			
-			$string = self::getHyphenator($language)->hyphenate($string);
-			
-			//Start - restore html-attributes
-				foreach ($matches as $index => $match) {
-					$string = preg_replace('|###'.$index.'###|', $match[0], $string, 1);
-				}
-			//End - restore html-attributes
-			
-			
-			
-			return $string;
-		}
-		
-		private static function getHyphenator($language) {
-			if (!isset(self::$hyphenators[$language])) {
-				$addon = rex_addon::get(__CLASS__);
-				
-				$h = \Org\Heigl\Hyphenator\Hyphenator::factory(null, self::getLocale($language));
-				$o = $h->getOptions();
-				
-				if (!empty($addon->getConfig('hyphen'))) {
-					$o->setHyphen($addon->getConfig('hyphen'));
-				} else {
-					$o->setHyphen('&shy;');
-				}
-				
-				//Bugfix for safari
-				//see: https://blog.decaf.de/2016/03/04/705665289569099777/
-				//if (stripos($_SERVER ['HTTP_USER_AGENT'], 'safari') !== false && stripos($_SERVER ['HTTP_USER_AGENT'], 'chrome') == false && $o->getHyphen() == '&shy;') {
-				//	$o->setHyphen('<i></i>&shy;');
-				//}
-				
-				if (!empty($addon->getConfig('leftMin'))) {
-					$o->setLeftMin($addon->getConfig('leftMin'));
-				} else {
-					$o->setLeftMin(2);
-				}
-				
-				if (!empty($addon->getConfig('rightMin'))) {
-					$o->setRightMin($addon->getConfig('rightMin'));
-				} else {
-					$o->setRightMin(2);
-				}
-				
-				if (!empty($addon->getConfig('wordMin'))) {
-					$o->setWordMin($addon->getConfig('wordMin'));
-				} else {
-					$o->setWordMin(6);
-				}
-				
-				$h->setOptions($o);
-				self::$hyphenators[$language] = $h;
-			}
-			
-			return self::$hyphenators[$language];
-		}
-		
-		private static function getLocale($language) {
-			switch ($language) {
-				case 'de':
-					$language = 'de_DE';
-				break;
-				case 'en':
-					$language = 'en_GB';
-				break;
-			}
-			
-			return $language;
-		}
-	}
-?>
+
+    class hyphenator
+    {
+        private static $hyphenators = [];
+
+        public static function hyphenate($string, $language = '')
+        {
+            //Start - save html-tags
+            preg_match_all('/<[^<]*>/', $string, $matches, PREG_SET_ORDER);
+            foreach ($matches as $index => $match) {
+                $string = preg_replace('|'.preg_quote($match[0], '|').'|', '###'.$index.'###', $string, 1);
+            }
+            //End - save html-attributes
+
+            if ('' == $language) {
+                $language = rex_clang::getCurrent()->getCode();
+            }
+
+            $string = self::getHyphenator($language)->hyphenate($string);
+
+            //Start - restore html-attributes
+            foreach ($matches as $index => $match) {
+                $string = preg_replace('|###'.$index.'###|', $match[0], $string, 1);
+            }
+            //End - restore html-attributes
+
+            return $string;
+        }
+
+        private static function getHyphenator($language)
+        {
+            if (!isset(self::$hyphenators[$language])) {
+                $addon = rex_addon::get(__CLASS__);
+
+                $h = \Org\Heigl\Hyphenator\Hyphenator::factory(null, self::getLocale($language));
+                $o = $h->getOptions();
+
+                if (!empty($addon->getConfig('hyphen'))) {
+                    $o->setHyphen($addon->getConfig('hyphen'));
+                } else {
+                    $o->setHyphen('&shy;');
+                }
+
+                //Bugfix for safari
+                //see: https://blog.decaf.de/2016/03/04/705665289569099777/
+                //if (stripos($_SERVER ['HTTP_USER_AGENT'], 'safari') !== false && stripos($_SERVER ['HTTP_USER_AGENT'], 'chrome') == false && $o->getHyphen() == '&shy;') {
+                //	$o->setHyphen('<i></i>&shy;');
+                //}
+
+                if (!empty($addon->getConfig('leftMin'))) {
+                    $o->setLeftMin($addon->getConfig('leftMin'));
+                } else {
+                    $o->setLeftMin(2);
+                }
+
+                if (!empty($addon->getConfig('rightMin'))) {
+                    $o->setRightMin($addon->getConfig('rightMin'));
+                } else {
+                    $o->setRightMin(2);
+                }
+
+                if (!empty($addon->getConfig('wordMin'))) {
+                    $o->setWordMin($addon->getConfig('wordMin'));
+                } else {
+                    $o->setWordMin(6);
+                }
+
+                $h->setOptions($o);
+                self::$hyphenators[$language] = $h;
+            }
+
+            return self::$hyphenators[$language];
+        }
+
+        private static function getLocale($language)
+        {
+            switch ($language) {
+                case 'de':
+                    $language = 'de_DE';
+                break;
+                case 'en':
+                    $language = 'en_GB';
+                break;
+            }
+
+            return $language;
+        }
+    }
